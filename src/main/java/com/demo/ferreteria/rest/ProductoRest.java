a2725d12d34298cf42479cdeaa65845849952143
Jose <isaac93amado@gmail.com>
2023-09-06 16:15:16 -0600

    agrego anotacion para consumir los servicios de manera local

--- src/main/java/com/demo/ferreteria/rest/ProductoRest.java
+++ src/main/java/com/demo/ferreteria/rest/ProductoRest.java
@@ -4,17 +4,16 @@ import com.demo.ferreteria.modelo.Producto;
 import com.demo.ferreteria.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/productos")
 public class ProductoRest {
 
+    //inyeccion de dependencias
     @Autowired
     private ProductoService productoService;
     /*Devuelve todos los productos*/
